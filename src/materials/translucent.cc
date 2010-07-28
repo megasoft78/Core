e5c587d5b8fcbf75170a7dcc73b877d1b3258b1a
liyong03 <liyong03@gmail.com>
2010-07-28 22:18:34 +0800

    combined glossy and diffuse to translucent material

--- src/materials/translucent.cc
+++ src/materials/translucent.cc
@@ -22,6 +22,7 @@
 #include <core_api/environment.h>
 #include <core_api/scene.h>
 #include <materials/maskmat.h>
+#include <materials/microfacet.h>
 //#include <yafraycore/spectrum.h>
 
 
@@ -31,6 +32,9 @@ just a test material for using material class
 
 __BEGIN_YAFRAY
 
+#define C_TRANSLUCENT 	0
+#define C_GLOSSY		1
+#define C_DIFFUSE		2
 
 struct TranslucentData_t
 {
@@ -59,6 +63,17 @@ class translucentMat_t: public nodeMaterial_t
 		shaderNode_t *diffuseS; //!< shader node for diffuse colo
 		color_t diffuseCol;
 		color_t specRefCol;
+		color_t gloss_color;
+	
+		float with_diffuse;
+		float mTransl, mDiffuse, mGlossy;
+		float pDiffuse;
+		float exponent;
+	
+		BSDF_t cFlags[3];
+		int nBSDF;
+	
+		// parameters for translucent property	
 		color_t sigma_s;
 		color_t sigma_a;
 		float	IOR;
@@ -67,8 +82,43 @@ class translucentMat_t: public nodeMaterial_t
 translucentMat_t::translucentMat_t(color_t diffuseC, color_t specC, color_t siga, color_t sigs, float ior): diffuseCol(diffuseC),specRefCol(specC),sigma_a(siga),sigma_s(sigs),IOR(ior),
 								diffuseS(0)
 {
-	bsdfFlags = BSDF_TRANSLUCENT;// | BSDF_SPECULAR;
-	//std::cout << sigma_a << " " << sigma_s << " " << IOR << std::endl;
+//	bsdfFlags = BSDF_TRANSLUCENT;// | BSDF_SPECULAR;
+//	//std::cout << sigma_a << " " << sigma_s << " " << IOR << std::endl;
+//	
+//	if(mDiffuse > 0)
+//	{
+//		bsdfFlags |= BSDF_DIFFUSE | BSDF_REFLECT;
+//		with_diffuse = true;
+//	}
+//	
+//	bsdfFlags |= (BSDF_GLOSSY | BSDF_REFLECT);
+	
+	gloss_color = color_t(1.0f);
+	
+	mDiffuse = 1.0f;
+	mGlossy = 0.99f;
+	mTransl = 1.0f;
+	
+	pDiffuse = std::min(0.6f , 1.f - (mGlossy/(mGlossy + (1.f-mGlossy)*mDiffuse)) );
+	
+	exponent = 200;
+	
+	cFlags[C_TRANSLUCENT] = (BSDF_TRANSLUCENT);
+	cFlags[C_GLOSSY] = (BSDF_GLOSSY | BSDF_REFLECT);
+	
+	if(mDiffuse>0)
+	{
+		cFlags[C_DIFFUSE] = BSDF_DIFFUSE | BSDF_REFLECT;
+		with_diffuse = true;
+		nBSDF = 3;
+	}
+	else
+	{
+		cFlags[C_DIFFUSE] = BSDF_NONE;
+		nBSDF = 2;
+	}
+	
+	bsdfFlags = cFlags[C_TRANSLUCENT] | cFlags[C_GLOSSY] | cFlags[C_DIFFUSE];
 }
 
 translucentMat_t::~translucentMat_t()
@@ -95,43 +145,300 @@ void translucentMat_t::initBSDF(const renderState_t &state, const surfacePoint_t
 
 color_t translucentMat_t::eval(const renderState_t &state, const surfacePoint_t &sp, const vector3d_t &wo, const vector3d_t &wl, BSDF_t bsdfs)const
 {
-	color_t scolor(0.f);
+//	color_t scolor(0.f);
+//	
+//	// face forward:
+//	vector3d_t N = FACE_FORWARD(sp.Ng, sp.N, wo);
+//	//if(!(bsdfs & bsdfFlags )) 
+//	//	return scolor;
+//	if(N*wl < 0.0) 
+//		return scolor;
+//	//if (bsdfs & BSDF_TRANSLUCENT) 
+//	{
+//		scolor = diffuseCol;
+//	}
+//	
+//	float Kr, Kt;
+//	fresnel(wl, N, IOR, Kr, Kt);
+//	
+//	return scolor*Kr;
 	
-	// face forward:
+	if( !(bsdfs & BSDF_DIFFUSE) || ((sp.Ng*wl)*(sp.Ng*wo)) < 0.f ) return color_t(0.f);
+	
+	//MDat_t *dat = (MDat_t *)state.userdata;
+	color_t col(0.f);
+	bool diffuse_flag = bsdfs & BSDF_DIFFUSE;
+	
+	//nodeStack_t stack(dat->stack);
 	vector3d_t N = FACE_FORWARD(sp.Ng, sp.N, wo);
-	//if(!(bsdfs & bsdfFlags )) 
-	//	return scolor;
-	if(N*wl < 0.0) 
-		return scolor;
-	//if (bsdfs & BSDF_TRANSLUCENT) 
-	{
-		scolor = diffuseCol;
-	}
+	
+	float wiN = std::fabs(wl * N);
+	float woN = std::fabs(wo * N);
 	
 	float Kr, Kt;
 	fresnel(wl, N, IOR, Kr, Kt);
 	
-	return scolor*Kr;
+	float mR = (1.0f - Kt*mTransl);
+	
+	if(bsdfs & BSDF_GLOSSY)
+	{
+		vector3d_t H = (wo + wl).normalize(); // half-angle
+		float cos_wi_H = std::max(0.f, wl*H);
+		float glossy;
+		
+		{
+			//glossy = Blinn_D(H*N, exponent) * SchlickFresnel(cos_wi_H, dat->mGlossy) / ASDivisor(cos_wi_H, woN, wiN);
+			glossy = mR*Blinn_D(H*N, exponent) * SchlickFresnel(cos_wi_H, this->mGlossy) / ASDivisor(cos_wi_H, woN, wiN);
+		}
+		
+		//col = glossy*(glossyS ? glossyS->getColor(stack) : gloss_color);
+		col = glossy*gloss_color;
+	}
+	
+	if(with_diffuse && diffuse_flag)
+	{
+		//col += diffuseReflect(wiN, woN, dat->mGlossy, dat->mDiffuse, (diffuseS ? diffuseS->getColor(stack) : diff_color)) * ((orenNayar)?OrenNayar(wi, wo, N):1.f);
+		col += mR * diffuseReflect(wiN, woN, this->mGlossy, this->mDiffuse, this->diffuseCol);
+	}
+	
+	return col;
 }
 
 color_t translucentMat_t::sample(const renderState_t &state, const surfacePoint_t &sp, const vector3d_t &wo, vector3d_t &wi, sample_t &s)const
 {
-	color_t scolor(0.f);
-	PFLOAT cos_Ng_wo = sp.Ng*wo, cos_Ng_wi;
+//	color_t scolor(0.f);
+//	PFLOAT cos_Ng_wo = sp.Ng*wo, cos_Ng_wi;
+//	
+//	vector3d_t N = FACE_FORWARD(sp.Ng, sp.N, wo);
+//	
+//	wi = SampleCosHemisphere(N, sp.NU, sp.NV, s.s1, s.s2);
+//	s.pdf = std::fabs(wi*N);
+//	
+//	cos_Ng_wi = sp.Ng*wi;
+//	if(cos_Ng_wo*cos_Ng_wi > 0)
+//		scolor = diffuseCol;
+//	
+//	float Kr, Kt;
+//	fresnel(wi, N, IOR, Kr, Kt);
+//	
+//	return scolor*Kr;
 	
-	vector3d_t N = FACE_FORWARD(sp.Ng, sp.N, wo);
+	{
+//	//MDat_t *dat = (MDat_t *)state.userdata;
+//	float cos_Ng_wo = sp.Ng*wo;
+//	float cos_Ng_wi;
+//	vector3d_t N = FACE_FORWARD(sp.Ng, sp.N, wo);//(cos_Ng_wo < 0) ? -sp.N : sp.N;
+//	vector3d_t Hs;
+//	s.pdf = 0.f;
+//	float wiN = 0.f;
+//	float woN = std::fabs(wo * N);
+//	float cos_wo_H = 0.f;
+//	
+//	color_t scolor(0.f);
+//	
+//	float s1 = s.s1;
+//	float cur_pDiffuse = pDiffuse;
+//	bool use_glossy = s.flags & BSDF_GLOSSY;
+//	bool use_diffuse = with_diffuse && (s.flags & BSDF_DIFFUSE);
+//	//nodeStack_t stack(dat->stack);
+//	float glossy = 0.f;
+//	
+//	if(use_diffuse)
+//	{
+//		float s_pDiffuse = use_glossy ? cur_pDiffuse : 1.f;
+//		if(s1 < s_pDiffuse)
+//		{
+//			s1 /= s_pDiffuse;
+//			wi = SampleCosHemisphere(N, sp.NU, sp.NV, s1, s.s2);
+//			
+//			cos_Ng_wi = sp.Ng * wi;
+//			
+//			if(cos_Ng_wi * cos_Ng_wo < 0.f) return scolor;
+//			
+//			wiN = std::fabs(wi * N);
+//			
+//			s.pdf = wiN;
+//			
+//			if(use_glossy)
+//			{
+//				vector3d_t H = (wi+wo).normalize();
+//				cos_wo_H = wo*H;
+//				float cos_wi_H = std::max(0.f, wi*H);
+//				float cos_N_H = N*H;
+//				{
+//					s.pdf = s.pdf*cur_pDiffuse + Blinn_Pdf(cos_N_H, cos_wo_H, exponent)*(1.f-cur_pDiffuse);
+//					glossy = Blinn_D(cos_N_H, exponent) * SchlickFresnel(cos_wi_H, mGlossy) / ASDivisor(cos_wi_H, woN, wiN);
+//				}
+//			}
+//			s.sampledFlags = BSDF_DIFFUSE | BSDF_REFLECT;
+//			
+//			if( !(s.flags & BSDF_REFLECT) ) return color_t(0.f);
+//			
+//			scolor = glossy*gloss_color;
+//			
+//			if(use_diffuse) scolor += diffuseReflect(wiN, woN, mGlossy, mDiffuse, diffuseCol);
+//			
+//			return scolor;
+//			
+//		}
+//		s1 -= cur_pDiffuse;
+//		s1 /= (1.f - cur_pDiffuse);
+//	}
+//	
+//	if(use_glossy)
+//	{
+//		{
+// 			Blinn_Sample(Hs, s1, s.s2, exponent);
+//			vector3d_t H = Hs.x*sp.NU + Hs.y*sp.NV + Hs.z*N;
+//			cos_wo_H = wo*H;
+//			if ( cos_wo_H < 0.f )
+//			{
+//				H = reflect_plane(N, H);
+//				cos_wo_H = wo*H;
+//			}
+//			// Compute incident direction by reflecting wo about H
+//			wi = reflect_dir(H, wo);
+//			cos_Ng_wi = sp.Ng*wi;
+//			
+//			if(cos_Ng_wo*cos_Ng_wi < 0.f) return color_t(0.f);
+//			
+//			wiN = std::fabs(wi * N);
+//			
+//			s.pdf = Blinn_Pdf(Hs.z, cos_wo_H, exponent);
+//			glossy = Blinn_D(Hs.z, exponent) * SchlickFresnel(cos_wo_H, mGlossy)  / ASDivisor(cos_wo_H, woN, wiN);
+//		}
+//		
+//		scolor = glossy * gloss_color;
+//		s.sampledFlags = BSDF_GLOSSY | BSDF_REFLECT;
+//	}
+//	
+//	if(use_diffuse)
+//	{
+//		s.pdf = wiN * cur_pDiffuse + s.pdf * (1.f-cur_pDiffuse);
+//		scolor += diffuseReflect(wiN, woN, mGlossy, mDiffuse, diffuseCol);
+//	}
+//	
+//	return scolor;
+	}
 	
-	wi = SampleCosHemisphere(N, sp.NU, sp.NV, s.s1, s.s2);
-	s.pdf = std::fabs(wi*N);
 	
-	cos_Ng_wi = sp.Ng*wi;
-	if(cos_Ng_wo*cos_Ng_wi > 0)
-		scolor = diffuseCol;
-	
+	//MDat_t *dat = (MDat_t *)state.userdata;
+	float cos_Ng_wo = sp.Ng*wo;
+	float cos_Ng_wi;
+	vector3d_t N = (cos_Ng_wo<0) ? -sp.N : sp.N;
+	vector3d_t Hs(0.f);
+	s.pdf = 0.f;
 	float Kr, Kt;
+	float wiN = 0.f , woN = 0.f;
+	
 	fresnel(wi, N, IOR, Kr, Kt);
 	
-	return scolor*Kr;
+	// missing! get components
+	//nodeStack_t stack(dat->stack);
+	bool use[3] = {false, false, false};
+	float sum = 0.f, accumC[3], val[3], width[3];
+	int cIndex[3]; // entry values: 0 := specular part, 1 := glossy part, 2:= diffuse part;
+	int rcIndex[3]; // reverse fmapping of cIndex, gives position of spec/glossy/diff in val/width array
+	accumC[0] = Kt*this->mTransl;
+	accumC[1] = (1.f - Kt*this->mTransl)*(1.f - pDiffuse);
+	accumC[2] = (1.f - Kt*this->mTransl)*(this->pDiffuse);
+	
+	int nMatch = 0, pick = -1;
+	for(int i = 0; i < nBSDF; ++i)
+	{
+		if((s.flags & cFlags[i]) == cFlags[i])
+		{
+			use[i] = true;
+			width[nMatch] = accumC[i];
+			cIndex[nMatch] = i;
+			rcIndex[i] = nMatch;
+			sum += width[nMatch];
+			val[nMatch] = sum;
+			++nMatch;
+		}
+	}
+	if(!nMatch || sum < 0.00001){ return color_t(0.f); }
+	else if(nMatch==1){ pick=0; width[0]=1.f; }
+	else
+	{
+		float inv_sum = 1.f/sum;
+		for(int i=0; i<nMatch; ++i)
+		{
+			val[i] *= inv_sum;
+			width[i] *= inv_sum;
+			if((s.s1 <= val[i]) && (pick<0 ))	pick = i;
+		}
+	}
+	if(pick<0) pick=nMatch-1;
+	float s1;
+	if(pick>0) s1 = (s.s1 - val[pick-1]) / width[pick];
+	else 	   s1 = s.s1 / width[pick];
+	
+	color_t scolor(0.f);
+	switch(cIndex[pick])
+	{
+		case C_TRANSLUCENT: // specular reflect
+			
+			break;
+		case C_GLOSSY: // glossy
+			Blinn_Sample(Hs, s1, s.s2, exponent);
+			break;
+		case C_DIFFUSE: // lambertian
+		default:
+			wi = SampleCosHemisphere(N, sp.NU, sp.NV, s1, s.s2);
+			cos_Ng_wi = sp.Ng*wi;
+			if(cos_Ng_wo*cos_Ng_wi < 0) return color_t(0.f);
+	}
+	
+	wiN = std::fabs(wi * N);
+	woN = std::fabs(wo * N);
+	
+	if(cIndex[pick] != C_TRANSLUCENT)
+	{
+		// evaluate BSDFs and PDFs...
+		if(use[C_GLOSSY])
+		{
+			PFLOAT glossy;
+			PFLOAT cos_wo_H;
+			if(cIndex[pick] != C_GLOSSY)
+			{
+				vector3d_t H = (wi+wo).normalize();
+				Hs = vector3d_t(H*sp.NU, H*sp.NV, H*N);
+				cos_wo_H = wo*H;
+			}
+			else
+			{
+				vector3d_t H = Hs.x*sp.NU + Hs.y*sp.NV + Hs.z*N;
+				cos_wo_H = wo*H;
+				if ( cos_wo_H < 0.f )
+				{
+					H = reflect_plane(N, H);
+					cos_wo_H = wo*H;
+				}
+				// Compute incident direction by reflecting wo about H
+				wi = reflect_dir(H, wo);
+				cos_Ng_wi = sp.Ng*wi;
+				if(cos_Ng_wo*cos_Ng_wi < 0) return color_t(0.f);
+			}
+			
+			wiN = std::fabs(wi * N);
+			
+			{
+				s.pdf += Blinn_Pdf(Hs.z, cos_wo_H, exponent) * width[rcIndex[C_GLOSSY]];
+				glossy = Blinn_D(Hs.z, exponent) * SchlickFresnel(cos_wo_H, mGlossy) / ASDivisor(cos_wo_H, woN, wiN);
+			}
+			scolor = (CFLOAT)glossy*(1.f-Kt*this->mTransl)*gloss_color;
+		}
+		
+		if(use[C_DIFFUSE])
+		{
+			scolor += (1.f-Kt*this->mTransl)*diffuseReflect(wiN, woN, mGlossy, mDiffuse, diffuseCol);
+			s.pdf += wiN * width[rcIndex[C_DIFFUSE]];
+		}
+	}
+	s.sampledFlags = cFlags[cIndex[pick]];
+	
+	return scolor;
 }
 
 color_t translucentMat_t::emit(const renderState_t &state, const surfacePoint_t &sp, const vector3d_t &wo)const
@@ -141,8 +448,80 @@ color_t translucentMat_t::emit(const renderState_t &state, const surfacePoint_t 
 
 float translucentMat_t::pdf(const renderState_t &state, const surfacePoint_t &sp, const vector3d_t &wo, const vector3d_t &wi, BSDF_t bsdfs)const
 {	
+//	vector3d_t N = FACE_FORWARD(sp.Ng, sp.N, wo);
+//	return std::fabs(wi*N);
+	{
+//	//MDat_t *dat = (MDat_t *)state.userdata;
+//	if((sp.Ng * wo) * (sp.Ng * wi) < 0.f) return 0.f;
+//	vector3d_t N = FACE_FORWARD(sp.Ng, sp.N, wo);
+//	float pdf = 0.f;
+//	float cos_wo_H = 0.f;
+//	float cos_N_H = 0.f;
+//	
+//	float cur_pDiffuse = pDiffuse;
+//	bool use_glossy = (bsdfs & BSDF_GLOSSY);
+//	bool use_diffuse = with_diffuse && (bsdfs & BSDF_DIFFUSE);
+//	
+//	if(use_diffuse)
+//	{
+//		pdf = std::fabs(wi*N);
+//		if(use_glossy)
+//		{
+//			vector3d_t H = (wi+wo).normalize();
+//			cos_wo_H = wo*H;
+//			cos_N_H = N*H;
+//			pdf = pdf*cur_pDiffuse + Blinn_Pdf(cos_N_H, cos_wo_H, exponent)*(1.f-cur_pDiffuse);
+//		}
+//		return pdf;
+//	}
+//	
+//	if(use_glossy)
+//	{
+//		vector3d_t H = (wi+wo).normalize();
+//		cos_wo_H = wo*H;
+//		cos_N_H = N*H;
+//		pdf = Blinn_Pdf(cos_N_H, cos_wo_H, exponent);
+//	}
+//	return pdf;
+	}
+	
+	//MDat_t *dat = (MDat_t *)state.userdata;
+	bool transmit = ((sp.Ng * wo) * (sp.Ng * wi)) < 0.f;
+	if(transmit) return 0.f;
 	vector3d_t N = FACE_FORWARD(sp.Ng, sp.N, wo);
-	return std::fabs(wi*N);
+	float pdf = 0.f;
+	CFLOAT Kr, Kt;
+	
+	fresnel(wi, N, IOR, Kr, Kt);
+	
+	float accumC[3], sum=0.f, width;
+	accumC[0] = Kt*this->mTransl;
+	accumC[1] = (1.f - Kt*this->mTransl)*(1.f - pDiffuse);
+	accumC[2] = (1.f - Kt*this->mTransl)*(this->pDiffuse);
+	
+	int nMatch=0;
+	for(int i=0; i<nBSDF; ++i)
+	{
+		if((bsdfs & cFlags[i]) == cFlags[i])
+		{
+			width = accumC[i];
+			sum += width;
+			if(i == C_GLOSSY)
+			{
+				vector3d_t H = (wi+wo).normalize();
+				PFLOAT cos_wo_H = wo*H;
+				PFLOAT cos_N_H = N*H;
+				pdf += Blinn_Pdf(cos_N_H, cos_wo_H, exponent) * width;
+			}
+			else if(i == C_DIFFUSE)
+			{
+				pdf += std::fabs(wi*N) * width;
+			}
+			++nMatch;
+		}
+	}
+	if(!nMatch || sum < 0.00001) return 0.f;
+	return pdf / sum;
 }
 
 void translucentMat_t::getSpecular(const renderState_t &state, const surfacePoint_t &sp, const vector3d_t &wo,
